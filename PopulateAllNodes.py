3c3fa5f22ce569b53154749b4ab8a482fcc4c536
Neogene <neogene@gmail.com>
2018-05-09 06:58:46 +0200

    First release.

---
+++ PopulateAllNodes.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+#TILE: HOUDINI POPULATES
+#AUTHOR: eng. ANDREA LEGANZA
+#HOUDINI VERSION: TESTED ON HOUDINI 16.0 AND 16.5
+#SCRIPT VERSION: 1.0
+#DESCRIPTION:
+# 1. create a geometry node
+#2. right click on it -> Type Propertis -> scripts
+#3. Load the script after downloading or select the OnUpdated voice from the dropdown menu and paste the code (remember to select PYTHON as language on the right area dropdown menu)
+
+import hou
+def populateWithAllNodes():
+
+    categories = hou.nodeTypeCategories()
+    
+    i = 0
+    
+    parent = hou.node("/obj/geo1")
+    
+    #if len(parent.allItems())>0:
+    #parent.deleteItems(parent.allItems()) #removes all children
+
+    print "Children deleted..."
+    
+    for category in categories.values():
+        node_types = category.nodeTypes()
+        #print "NODE TYPES:"+node_types
+        for node_type in node_types.keys():
+            print "Creating: "+node_type 
+            
+            if node_type=="v_constant":
+                continue
+                
+            try:
+                node = parent.createNode(node_type)
+                i=i+1
+                if len(node.comment())==0 :
+                    node.destroy()
+                
+            except hou.OperationFailed:
+                print "error creating:"+node_type
+
+    print ("PARENT:"+parent.name())
+
+populateWithAllNodes()
--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# HoudiniGenerateAllNodes
-A simple script which populates the whole scene with all the nodes available in Houdini.
+# HoudiniHelpToComment
+A simple script which populates the whole scene with all the nodes available in Houdini
+
+## AUTHOR: ## 
+eng. ANDREA LEGANZA
+
+## TESTED VERSION/S: ## 
+HOUDINI 16/16.5
+
+## DESCRIPTION: ## 
+The script let you generate 
+
+## WORKS WITH: ##
+My script to use node documentation as node comment [Download](https://github.com/Neogene/HoudiniHelpToComment).
+
+## RESULTS: ##
+NOTE: comments are added automatically to every node using my script to use node documentation as node comment [Download](https://github.com/Neogene/HoudiniHelpToComment)
+
+![Script result](https://github.com/Neogene/HoudiniGenerateAllNodes/result.png)
+
+
+## INSTALLATION: ##
+1. create a geometry node
+2. right click on it -> Type Propertis -> scripts
+3. Load the script after downloading or select the OnUpdated voice from the dropdown menu and paste the code (remember to select PYTHON as language on the right area dropdown menu)
+
+## CONTRIBUTING: ##
+Please feel free to optimize the script. 
